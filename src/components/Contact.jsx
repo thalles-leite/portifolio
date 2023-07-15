06a1d0749ac32dd181af7593113ee26eedb646b8
Thalles Leite <eng.thallesleite@gmail.com>
2023-07-15 15:24:29 -0300

    Ajuste de css.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,20 +8,18 @@ import { AiOutlineMail } from 'react-icons/ai';
 
 export default function Contact() {
   return (
-    <section className="text-gray-800 pt-10 px-4 flex flex-col shadow-lg items-center   relative" id="contact">
+    <section className=" text-gray-800 pt-10 px-4 flex flex-col shadow-lg items-center   relative" id="contact">
 
-      <section className="flex text-justify justify-center mb-10 max-w-7xl flex-col items-center mt-10">
+      <section className="flex text-justify justify-center mb-10 w-full flex-col items-center mt-10">
         <section
-          data-aos="fade-left"
-          data-aos-duration="1000"
-          className="h-fit"
+          className="h-fit w-full "
         >
           <h1 className="text-2xl font-bold text-center justify-center flex items-center gap-2 mb-10">
             <FaMailBulk className="w-6 h-6 text-gray-800" />
             Contato
           </h1>
 
-          <section className="text-gray-800 pt-10  min-w-full px-4 flex flex-col gap-10 shadow-lg  items-center   relative p-5 rounded-2xl" id="contact">
+          <section className=" bg-white text-gray-800 pt-10   px-4 flex flex-col gap-10 shadow-lg  items-center   relative p-5 rounded-2xl" id="contact">
             <section className="flex flex-row items-center justify-center gap-10">
               <section className="flex flex-col items-center justify-center gap">
                 <h1 className="text-2xl font-bold text-center flex items-center gap-2 mb-2">
