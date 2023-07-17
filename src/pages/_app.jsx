ed6133e8b2ad4767871febe47a5a04a836fc6549
thalles-leite <eng.thallesleite@gmail.com>
2023-07-17 16:03:33 -0300

    Adição do vercel analytucs,

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 import '../styles/globals.css';
 import React, { useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 import initializeAOS from '../utils/aos';
 import 'aos/dist/aos.css';
@@ -11,5 +13,11 @@ export default function App({ Component, pageProps }) {
     initializeAOS(); // Inicialize o AOS
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+
+  );
 }
